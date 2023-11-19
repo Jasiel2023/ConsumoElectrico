b5a4258a29b07df56d56739ae7264c72f44d1319
Wilmer <wilmer.pardo@unl.edu.ec>
2023-11-19 16:11:27 -0500

    Cambios en clases

--- src/Informe.java
+++ src/Informe.java
@@ -2,19 +2,46 @@ import javax.xml.crypto.Data;
 import java.util.Date;
 
 public class Informe {
-    private float cantidadConsumo;
+    private float cantidadConsumo = 0;
     private Date fechaAnalisis;
     private String infoUsuario;
     private String resultadosAlmacenados;
-    protected void precioConsumoElectrico(){
 
-
+    public float getCantidadConsumo() {
+        return cantidadConsumo;
     }
+
+    public void setFechaAnalisis(Date fechaAnalisis) {
+        this.fechaAnalisis = new Date();
+    }
+
+    public void setInfoUsuario(String infoUsuario) {
+        this.infoUsuario = usuario.nombre + " " + usuario.apellido + " " + usuario.cedula;
+    }
+
+
+    
+
+
+    @Override
+    public String toString() {
+        return "Informe [cantidadConsumo=" + cantidadConsumo + ", fechaAnalisis=" + fechaAnalisis + ", infoUsuario="
+                + infoUsuario + ", resultadosAlmacenados=" + resultadosAlmacenados + "]";
+    }
+
+
+
     public void generarInforme(){
-
+        System.out.println("Generando informe..."
+                + "\nCantidad de consumo: " + cantidadConsumo
+                + "\nFecha de análisis: " + fechaAnalisis
+                + "\nInformación del usuario: " + infoUsuario
+                + "\nResultados almacenados: " + resultadosAlmacenados);
+                if ((MedidorDeConsumo.analizarDatos() == true)){
+                    System.out.println("Alerta de consumo excesivo");
+                }
     }
-    public boolean alertaSobreConsumo(){
 
-        return true;
-    }
+    Usuario usuario = new Usuario("Cajas", "0705765628", "Av. 6 de Diciembre", "Richar");
+    MedidorDeConsumo medidorDeConsumo;
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-   
+        Usuario usuario1 = new Usuario("Cajas", "0705765628", "Av. 6 de Diciembre", "Richar");
+        Informe informe1 = new Informe();
+       // System.out.println("Usuario: " + usuario1.getNombre() + " " + usuario1.getApellido() + " " + usuario1.getCedula());
+        informe1.setInfoUsuario(usuario1.getNombre() + usuario1.getApellido() + usuario1.getCedula());
+        System.out.println(informe1);
     }
 }
